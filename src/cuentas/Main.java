f79a18db3a55305c7e9e8ad343f7bdea3a446e91
Tomeitas <1Hari1Seldon1@gmail.com>
2021-02-20 15:05:17 +0100

    Añado comentarios de prueba y modifico el texto que indica el saldo en cuenta.

--- src/cuentas/Main.java
+++ src/cuentas/Main.java
@@ -1,14 +1,19 @@
 package cuentas;
 
+/**
+ * Introduzco un comentario JavaDoc para probar Git
+ * @author Diego
+ */
 public class Main {
 
     public static void main(String[] args) {
-        CCuenta cuenta1;
         double saldoActual;
-
-        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        
+        // Elimino y modifico líneas para probar Git
+        CCuenta cuenta1 = 
+                new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
+        System.out.println("El saldo en cuenta es "+ saldoActual );
 
         operativa_cuenta(cuenta1, 0.0F);
     }
